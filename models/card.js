c00154ff2792a5b6c66a9c42b284d2d3c91312f3
Tsykunov Oleksandr <50213824+AlexThukynov1@users.noreply.github.com>
2023-11-02 18:33:25 +0200

    Add files via upload

--- models/card.js
+++ models/card.js
@@ -1,13 +1,46 @@
+const path = require("path");
+const fs = require("fs");
+
+const p = path.join(__dirname, '..',  "data", "card.json");
+
 class Card {
+  static async add(course) {
+    const card = await Card.fetch();
 
-    add() {
-        console.log('add')
+    const index = card.courses.findIndex((c) => c.id === course.id);
+    const possibleCourse = card.courses[index];
+
+    if (possibleCourse) {
+      possibleCourse.count++;
+      card.courses[index] = possibleCourse;
+    } else {
+      (course.count = 1), card.courses.push(course);
     }
 
- async   fetch() {
-        console.log('fetch')
-    }
+    card.price += +course.price;
 
+    return new Promise((resolve, reject) => {
+      fs.writeFile(p, JSON.stringify(card), (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
+
+  static async fetch() {
+    return new Promise((resolve, reject) => {
+      fs.readFile(p, "utf-8", (err, content) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(JSON.parse(content));
+        }
+      });
+    });
+  }
 }
 
-module.exports = Card
+module.exports = Card;
